841ab22ddfe8d00d304c9560d409fa286e49d965
alanusse <lanusseagustin@gmail.com>
2020-09-23 17:56:29 -0300

    Updates

---
+++ precache-manifest.b734b27b66155b79aaeec309bfc5acd5.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a42364585cb25822ef93a3ecb6e8003",
+    "url": "/react-weather-app/index.html"
+  },
+  {
+    "revision": "1c7a363959df9cdc4bae",
+    "url": "/react-weather-app/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "4a707ac269fd37a10a42",
+    "url": "/react-weather-app/static/css/main.446155bf.chunk.css"
+  },
+  {
+    "revision": "1c7a363959df9cdc4bae",
+    "url": "/react-weather-app/static/js/2.7130bcb2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-weather-app/static/js/2.7130bcb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a707ac269fd37a10a42",
+    "url": "/react-weather-app/static/js/main.7ccab1df.chunk.js"
+  },
+  {
+    "revision": "a165d426a6e734b12112",
+    "url": "/react-weather-app/static/js/runtime-main.ec1803de.js"
+  },
+  {
+    "revision": "cb7f1d9597e52872b28950c8d14dd7b6",
+    "url": "/react-weather-app/static/media/cloud.cb7f1d95.svg"
+  },
+  {
+    "revision": "cfb1e955a2bc8de4cda78aca20d0a996",
+    "url": "/react-weather-app/static/media/cloudy-1.cfb1e955.svg"
+  },
+  {
+    "revision": "6de435033dd76bd040771b6dbac9505e",
+    "url": "/react-weather-app/static/media/night-1.6de43503.svg"
+  },
+  {
+    "revision": "23e71eddf6c1ac2aedea1c3a485e41fc",
+    "url": "/react-weather-app/static/media/rain-1.23e71edd.svg"
+  },
+  {
+    "revision": "5ee170bc5e43292f72a181905bdf9d5b",
+    "url": "/react-weather-app/static/media/rain-2.5ee170bc.svg"
+  },
+  {
+    "revision": "20df6662a50275d0e84cbb4f10740084",
+    "url": "/react-weather-app/static/media/snowing-3.20df6662.svg"
+  },
+  {
+    "revision": "4b969a4c6cc99b09168f1458b5cf3499",
+    "url": "/react-weather-app/static/media/storm.4b969a4c.svg"
+  },
+  {
+    "revision": "3fe3e19ce073be595b8b77379101b0e9",
+    "url": "/react-weather-app/static/media/summer.3fe3e19c.svg"
+  },
+  {
+    "revision": "ef790a4b785f3ef1e86b614e0c35a97e",
+    "url": "/react-weather-app/static/media/sun.ef790a4b.svg"
+  },
+  {
+    "revision": "f511bc840e7c9d6291f4011a8f9909ad",
+    "url": "/react-weather-app/static/media/tornado.f511bc84.svg"
+  },
+  {
+    "revision": "19c88f05b6211833a44dade1f4337a2f",
+    "url": "/react-weather-app/static/media/winter.19c88f05.svg"
+  }
+]);
